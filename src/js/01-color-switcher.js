a37e3989f15e3945f4b7cf2cd2ad0d592302ccf2
Sopelniak <106702004+Sopelniak@users.noreply.github.com>
2022-09-10 23:18:59 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,17 @@
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStopEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+btnStartEl.addEventListener('click', onBtnStartClick);
+// btnStopEl.addEventListener('click', onBtnStopClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function onBtnStartClick() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  console.log(bodyEl.style.backgroundColor);
+}
