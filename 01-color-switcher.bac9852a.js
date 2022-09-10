3b5ad86d2ec3bbddd97c9b44eb83df7830e04026
Sopelniak <106702004+Sopelniak@users.noreply.github.com>
2022-09-10 20:19:27 +0000

    Deploying to gh-pages from @ Sopelniak/goit-js-hw-09@a37e3989f15e3945f4b7cf2cd2ad0d592302ccf2 🚀

---
+++ 01-color-switcher.bac9852a.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),o=(document.querySelector("button[data-stop]"),document.querySelector("body"));t.addEventListener("click",(function(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),console.log(o.style.backgroundColor)}))}();
+//# sourceMappingURL=01-color-switcher.bac9852a.js.map
