5a77b07c48ecc5ef613c7e5d7e1cba0a23651d82
Sopelniak <106702004+Sopelniak@users.noreply.github.com>
2022-09-11 00:11:22 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,9 +1,18 @@
 const btnStartEl = document.querySelector('button[data-start]');
 const btnStopEl = document.querySelector('button[data-stop]');
 const bodyEl = document.querySelector('body');
+let timerId = null;
 
 btnStartEl.addEventListener('click', onBtnStartClick);
-// btnStopEl.addEventListener('click', onBtnStopClick);
+btnStopEl.addEventListener('click', onBtnStopClick);
+
+function disabledBtn(btn) {
+  btn.disabled = true;
+}
+
+function anabledBtn(btn) {
+  btn.disabled = false;
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
@@ -12,6 +21,17 @@ function getRandomHexColor() {
 }
 
 function onBtnStartClick() {
-  bodyEl.style.backgroundColor = getRandomHexColor();
-  console.log(bodyEl.style.backgroundColor);
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  disabledBtn(btnStartEl);
+  anabledBtn(btnStopEl);
 }
+
+function onBtnStopClick() {
+  clearInterval(timerId);
+  anabledBtn(btnStartEl);
+  disabledBtn(btnStopEl);
+}
+
+disabledBtn(btnStopEl);
