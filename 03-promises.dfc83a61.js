a669130867a6581850e93daf0df328d2e89bced2
Sopelniak <106702004+Sopelniak@users.noreply.github.com>
2022-09-11 22:05:42 +0000

    Deploying to gh-pages from @ Sopelniak/goit-js-hw-09@1104f2deaeb6aaf46718c2ac273305ad7a3f3913 🚀

---
+++ 03-promises.dfc83a61.js
@@ -0,0 +1,2 @@
+var e;document.querySelector(".form button").addEventListener("submit",void(timerId=setInterval((()=>{e.preventDefault();const{elements:{delay:t,step:a,amount:n}}=e.currentTarget,r={};r[t.name]=t.value,r[a.name]=a.value,r[n.name]=n.value,position,Math.random()}),1e3)));
+//# sourceMappingURL=03-promises.dfc83a61.js.map
