1104f2deaeb6aaf46718c2ac273305ad7a3f3913
Sopelniak <106702004+Sopelniak@users.noreply.github.com>
2022-09-12 01:05:12 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,23 @@
+const btnSubmitEl = document.querySelector('.form button');
+
+btnSubmitEl.addEventListener('submit', onBtnSubmitEl());
+
+function onBtnSubmitEl(event) {
+  timerId = setInterval(() => {
+    event.preventDefault();
+    const {
+      elements: { delay, step, amount },
+    } = event.currentTarget;
+
+    const promiseValues = {};
+    promiseValues[delay.name] = delay.value;
+    promiseValues[step.name] = step.value;
+    promiseValues[amount.name] = amount.value;
+
+    createPromise(position, delay);
+  }, 1000);
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
